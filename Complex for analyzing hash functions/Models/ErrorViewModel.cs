8831ddff57b1879a4f2738545737c38380df622f
VladislavGrach <vladislav.grachev.03@mail.ru>
2025-11-26 16:12:49 +0300

    Добавьте файлы проекта.

---
+++ Complex for analyzing hash functions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Complex_for_analyzing_hash_functions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
